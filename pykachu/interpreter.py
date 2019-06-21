be8f277a7bb4d3417a21ebd37e53c1b2602ff363
dany.lewin <dany.lewin@forter.com>
2019-06-21 10:29:29 +0300

    Slight refactoring

--- pykachu/interpreter.py
+++ pykachu/interpreter.py
@@ -4,25 +4,24 @@ Methods:
 run -- The main context for the pikachu vm.
 """
 
-import sys
 from pykachu.utils import pika_error, pika_print
 from pykachu.reader import PikaReader
 from pykachu.stack import PikaStack
 
 
-def run(fileName, args, debug):
+def run(file_name, args, debug):
     """
     Run a specified Pikachu file in a virtual environment.
 
     Arguments:
-    fileName -- the name and path of a file containing a pikachu program.
+    file_name -- the name and path of a file containing a pikachu program.
     args -- the command line arguments specified when the pikachu interpreter was
     run.
     """
     pi_stack = PikaStack()
     pika_stack = PikaStack()
 
-    stackDict = {
+    stacks_dict = {
         "pi pikachu": pi_stack,
         "pika pikachu": pika_stack
     }
@@ -30,7 +29,7 @@ def run(fileName, args, debug):
     for a in args:
         pi_stack.PUSH(a)
 
-    reader = PikaReader(fileName)
+    reader = PikaReader(file_name)
     while True:
         try:
             if debug:
@@ -84,55 +83,44 @@ def run(fileName, args, debug):
                 pika_error(reader.line_num, 'unknown command "{}"'.format(reader.lines[reader.line_num]))
         elif len(terms) < 4:
             try:
-                tStack = stackDict[" ".join(terms[-2:])]
+                current_stack = stacks_dict[" ".join(terms[-2:])]
             except KeyError:
                 pika_error(reader.line_num, 'unknown pikachu "{}"'.format(" ".join(terms[-2:])))
             command = terms[0]
             if command == "pikachu":
-                tStack.DIV()
+                current_stack.DIV()
+                if current_stack.PEEK() == float('NaN'):
+                    pika_error(reader.line_num, 'cannot divide by 0')
             else:
-                tStack.PUSH(1)
+                current_stack.PUSH(1)
         elif len(terms) < 5:
             try:
-                tStack = stackDict[" ".join(terms[-2:])]
+                current_stack = stacks_dict[" ".join(terms[-2:])]
             except KeyError:
                 pika_error(reader.line_num, 'unknown pikachu "{}"'.format(" ".join(terms[-2:])))
             command = " ".join(terms[:-2])
             if command == "pi pika":
-                tStack.ADD()
+                current_stack.ADD()
             elif command == "pika pi":
-                tStack.SUB()
+                current_stack.SUB()
             elif command == "pi pikachu":
-                tStack.MULT()
+                current_stack.MULT()
             elif command == "pika pikachu":
-                if not tStack.EMPTY():
-                    pika_print(tStack.POP())
+                if not current_stack.EMPTY():
+                    pika_print(current_stack.POP())
                 else:
                     pika_print("undefined")
             elif command == "pikachu pikachu":
-                n = tStack.POP()
+                n = current_stack.POP()
                 if n and type(n) == int:
                     pika_print(chr(n))
                 else:
                     pika_print("undefined")
             else:
-                tStack.PUSH(2)
+                current_stack.PUSH(2)
         else:
             try:
-                tStack = stackDict[" ".join(terms[-2:])]
+                current_stack = stacks_dict[" ".join(terms[-2:])]
             except KeyError:
                 pika_error(reader.line_num, 'unknown pikachu "{}"'.format(" ".join(terms[-2:])))
-            tStack.PUSH(len(terms) - 2)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        pika_print("No Pika file specified")
-        exit()
-    fileName = sys.argv[1]
-    try:
-        args = [int(x) for x in sys.argv[2:]]
-    except ValueError:
-        pika_print("invalid argument list: ", sys.argv[2:])
-        exit()
-    run(fileName, args)
+            current_stack.PUSH(len(terms) - 2)
