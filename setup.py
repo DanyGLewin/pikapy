5a2c95f5b82315ccce4920f41376a9e77e5751bf
dany.lewin <dany.lewin@forter.com>
2019-06-21 17:35:53 +0300

    Added README to pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='pikapy',  # How you named your package folder (MyLib)
     packages=['pikapy'],  # Chose the same as "name"
-    version='0.3',  # Start with a small number and increase it with every change you make
+    version='0.4',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Pikachu interpreter written in Python',  # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Dany Lewin',  # Type in your name
     author_email='danyglewin@gmail.com',  # Type in your E-Mail
     url='https://github.com/DanyGLewin/pykachu',  # Provide either the link to your github or to your website
-    download_url='https://github.com/DanyGLewin/pykachu/archive/V0.3.tar.gz',  # I explain this later on
+    download_url='https://github.com/DanyGLewin/pykachu/archive/V0.4.tar.gz',  # I explain this later on
     keywords=['Esolang', 'Esoteric', 'Language', 'Pokemon', 'Pikachu', 'Interpreter'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'click',
